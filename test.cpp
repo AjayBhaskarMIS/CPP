710b750685ce03cd43ec9b2be5585ab9ffe6c701
AjayBhaskarMIS <athi.bhaskar@moveinsync.com>
2025-03-04 11:44:29 +0530

    Testing the git

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//   int x = 5;
+//   int y = 10;
+int x,y,z;
+cin>>x>>y>>z;
+
+  int sum = x + y;
+
+  std::cout << "The sum of " << x << " and " << y << " is: " << sum << std::endl;
+    cout<<"z is: "<<z<<endl;
+  return 0;
+}
